205c57f62bc770433285caae353ff2763f5e3e6c
Miguel Ramos <return.infox@gmail.com>
2024-12-03 16:09:06 -0300

    state de carrito completado

--- redux/slices/cartSlice.ts
+++ redux/slices/cartSlice.ts
@@ -27,10 +27,16 @@ const cartSlice = createSlice({
         // Si no existe, lo agregamos como nuevo item
         state.items.push(action.payload)
       }
+    },
+    removeFromCart(state, action:PayloadAction<number>) {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+    clearCart(state) {
+      state.items = [];
     }
   }
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, clearCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
