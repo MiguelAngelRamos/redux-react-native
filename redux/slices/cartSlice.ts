c0d8f6a15d22b608f4ffa3f16ccbd99cadf2caaf
Miguel Ramos <return.infox@gmail.com>
2024-12-03 15:41:05 -0300

    redux configuracion de store y estado del carrito

---
+++ redux/slices/cartSlice.ts
@@ -0,0 +1,36 @@
+import { IProduct } from '@/types/IProduct';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+interface ICartItem extends IProduct {
+  quantity: number; // cantidad del producto del carrito
+}
+
+interface ICartState {
+  items: ICartItem[]
+}
+
+const initialState: ICartState = {
+  items: [],
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<ICartItem> ) {
+      const existingItem = state.items.find((item) =>item.id === action.payload.id)
+      if(existingItem) {
+        // Si existe aumentamos la cantidad
+        // existingItem.quantity = existingItem.quantity + action.payload.quantity;
+        existingItem.quantity += action.payload.quantity;
+      } else {
+        // Si no existe, lo agregamos como nuevo item
+        state.items.push(action.payload)
+      }
+    }
+  }
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore} from '@reduxjs/toolkit';
 
+
+// Los reducers van a manejar partes especificas de nuestro estado global
+
+import cartReducer from './slices/cartSlice';
 export const store = configureStore({
   reducer: {
-    
+    cart: cartReducer
   }
 });
