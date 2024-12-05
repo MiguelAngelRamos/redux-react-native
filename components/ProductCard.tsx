4f3576216dbe0b8740f539e60ee132c38cc2cebc
Miguel Ramos <return.infox@gmail.com>
2024-12-05 15:13:19 -0300

    productos de la lista terminado

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 
 
@@ -12,10 +12,44 @@ interface IProductCardProps {
 
 const ProductCard: React.FC<IProductCardProps> = ({title, price, image, onPress}) => {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.cardContainer}>
+      <TouchableOpacity>
+        <Image 
+          source={{uri: image}}
+          style={styles.image}
+          resizeMode='contain'
+        />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.price}>{price}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 16,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    shadowColor: '#000',
+    margin: 8,
+    flex: 1
+  },
+  image: {
+    width: '100%',
+    height: 160,
+    borderRadius: 8
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8
+  },
+  price: {
+    marginTop: 4,
+    fontSize: 19,
+    color: '#6b7280'
+  }
+})
 
 export default ProductCard
