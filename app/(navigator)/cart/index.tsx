df74bd3a813ace7641540204f9ac3719cb5d4c5f
Miguel Ramos <return.infox@gmail.com>
2024-12-10 15:00:23 -0300

    detalle del carrito de compra

--- app/(navigator)/cart/index.tsx
+++ app/(navigator)/cart/index.tsx
@@ -8,15 +8,15 @@ const Cart = () => {
   const cartItems = useSelector((state:RootState) => state.cart.items);
   const dispatch = useDispatch();
   return (
-    <View>
+    <View className="flex-1 bg-gray-100 p-4">
+      <Text className="text-2xl font-bold mb-4">Detalle de carrito de compra</Text>
       <FlatList 
         data={cartItems}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({item}) => (
-          <View>
+          <View className="flex-row justify-between items-center p-4 bg-white rounded-lg mb-2 shadow-slate-400">
             <Text>{item.title}</Text>
-            <Text>${item.price}</Text>
-            <Text>cantidad: {item.quantity}</Text>
+            <Text>${item.price} x {item.quantity}</Text>
           </View>
         )}
       />
